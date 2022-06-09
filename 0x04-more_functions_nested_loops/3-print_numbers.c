2bd092a0c9ad3edb2e55e1ab855d2868b7473744
JohnOparaji <opzynero@gmail.com>
2022-06-09 16:24:09 +0100

    More functions 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
 
 void print_numbers(void)
 {
-    int i;
+	int i;
 
-    for (i = 48; i < 58; i++)
-    {
-    	_putchar(i);
-    }
-    _putchar(10);
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
 }
